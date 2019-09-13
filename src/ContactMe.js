2ef86621b7094405bb14f1ede4f5d59fbc08b122
jsevern94 <severnj@spu.edu>
2019-09-12 23:52:11 -0700

    contact me and about me section filled out

--- src/ContactMe.js
+++ src/ContactMe.js
@@ -2,11 +2,42 @@ import React, { Component } from 'react';
 
 class ContactMe extends Component {
     render() {
+        const styles = {
+            content: {
+                width: '100vw',
+                justifyContent: 'center',
+                alignContent: 'center',
+                display: 'flex',
+                flexDirection: 'column',
+                marginTop: '5rem'
+
+            },
+            line: {
+                color: 'white',
+                fontSize: '1.75rem',
+                marginTop: '2rem',
+                marginBottom: '2rem'
+            },
+            link: {
+                color: '#a3bffa',
+                textDecoration: 'underline'
+            }
+        }
         return (
-            < div style={{ marginTop: '30px', fontSize: '30px' }
-            }>
-                Contact Me
-            </div >
+            <div>
+                < div style={{ marginTop: '30px', fontSize: '30px' }
+                }>
+                    Contact Me
+                </div>
+                <div style={styles.content}>
+                   <div style={styles.line}>Phone: 360.643.0442</div>
+                   <div style={styles.line}>Email: contact@jonahsevern.com</div>
+                   <div style={styles.line}>LinkedIn: <a href="https://www.linkedin.com/in/jonah-severn-12243aa5/" rel="noopener noreferrer" target="_blank" style={styles.link}>Jonah Severn</a></div>
+                   <div style={styles.line}>Github: <a href="https://github.com/jsevern94" rel="noopener noreferrer" target="_blank" style={styles.link}>jsevern94</a></div>
+         
+                </div>
+
+            </div>
         )
     }
 }
