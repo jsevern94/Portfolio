bd1c07b2405f14cab31a18980c8fa97a67b90c1d
jsevern94 <severnj@spu.edu>
2019-09-12 19:59:46 -0700

    put cards in a flex div

--- src/FeaturedApps.js
+++ src/FeaturedApps.js
@@ -3,15 +3,25 @@ import ProjectCard from './ProjectCard';
 
 class FeaturedApps extends Component {
     render() {
+        const styles = {
+            cards: {
+                justifyContent: 'center',
+                alignItems: 'center',
+                display: 'flex',
+                flexWrap: 'wrap'
+            }
+        }
         return (
             <div>
                 < div style={{ marginTop: '30px' }
                 }>
                     Featured Apps
-            </div >
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
+                </div >
+                <div style={styles.cards}>
+                    <ProjectCard />
+                    <ProjectCard />
+                    <ProjectCard />
+                </div>
             </div>
         )
     }
